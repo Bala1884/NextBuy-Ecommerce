b6eff4f8680a0afceeaa50b6cb0762142f1cfc59
Balamurugan R <shakthibala1884@gmail.com>
2025-07-02 17:56:24 +0530

    First commit

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-export default{
-  "development" :{
-    "username": "postgres",
-    "password": "Postgres123",
-    "database": "ecommerce_db",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-}
